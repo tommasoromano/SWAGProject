8bb6f2d6e58a763f8581faa000c3efcbacefd3ba
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2021-11-16 17:22:23 +0100

    comuni

--- Maven/src/main/java/Comune.java
+++ Maven/src/main/java/Comune.java
@@ -1,11 +1,123 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystems;
+import java.util.Scanner;
+
 public class Comune {
 
 	private String comune;
+	private String code;
 	
 	public Comune(String comune) {
 		
 		this.comune = comune;
 		
+		// associa comune con suo codice
+		File myObj = new File(FilePath());
+		Scanner myReader = null;
+		try {
+			myReader = new Scanner(myObj);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String line = myReader.nextLine();
+		while (myReader.hasNextLine()) {
+			line = myReader.nextLine();
+			String[] attr = line.split(",");
+			String c = attr[5].split("/")[0];
+			if (comune.equals(c)) {
+				this.code = attr[4];
+			}
+			
+		}
+		
+	}
+	
+	public static String[] GetAllRegion() {
+		
+		File myObj = new File(FilePath());
+		Scanner myReader = null;
+		try {
+			myReader = new Scanner(myObj);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String regione = "";
+		int regCode = 0;
+		String line = myReader.nextLine();
+		while (myReader.hasNextLine()) {
+			line = myReader.nextLine();
+			String[] attr = line.split(",");
+			if (Integer.parseInt(attr[1]) != regCode) {
+				regione = regione + "," + attr[2].split("/")[0];
+				regCode++;
+			}
+		}
+		
+		return regione.split(",");
+		
+	}
+	
+	public static String[] GetAllProvince(String regione) {
+		
+		File myObj = new File(FilePath());
+		Scanner myReader = null;
+		try {
+			myReader = new Scanner(myObj);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String provincia = "";
+		String last = "";
+		String line = myReader.nextLine();
+		while (myReader.hasNextLine()) {
+			line = myReader.nextLine();
+			String[] attr = line.split(",");
+			String p = attr[3].split("/")[0];
+			if (!last.equals(p)) {
+				if (regione.equals(attr[2])) {
+					provincia = provincia + "," + p;
+					last = p;
+				}
+			}
+		}
+		
+		return provincia.split(",");
+		
+	}
+	
+	public static String[] GetAllComuni(String provincia) {
+		
+		File myObj = new File(FilePath());
+		Scanner myReader = null;
+		try {
+			myReader = new Scanner(myObj);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String comuni = "";
+		String last = "";
+		String line = myReader.nextLine();
+		while (myReader.hasNextLine()) {
+			line = myReader.nextLine();
+			String[] attr = line.split(",");
+			String c = attr[5].split("/")[0];
+			if (!last.equals(c)) {
+				if (provincia.equals(attr[3])) {
+					comuni = comuni + "," + c;
+					last = c;
+				}
+			}
+		}
+		
+		return comuni.split(",");
+		
+	}
+	
+
+	private static String FilePath() {
+		return FileSystems.getDefault().getPath("").toAbsolutePath().toString()
+				+ "/src/main/java/comuni-italiani.csv";
 	}
 	
 }
--- Maven/src/main/java/Main.java
+++ Maven/src/main/java/Main.java
@@ -1,7 +1,17 @@
 
 public class Main {
 	public static void main(String args[]) {
-		CodiceFiscale c = new CodiceFiscale("RMNTMS00R03F205E".toCharArray());
-		System.out.println(CodiceFiscale.controllaValidita("RMNTMS00R03F205E".toCharArray()));
+		String[] r = Comune.GetAllRegion();
+		for (int i = 0; i < r.length; i++) {
+			System.out.println(r[i]);
+			String[] p = Comune.GetAllProvince(r[i]);
+			for (int j = 0; j < p.length; j++) {
+				System.out.println(p[j]);
+				String[] c = Comune.GetAllComuni(p[j]);
+				for (int k = 0; k < c.length; k++) {
+					//System.out.print(c[k]);
+				}
+			}
+		}
 	}
 }
