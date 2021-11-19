8b828e9e72940aa9838436c3269c708480b58c68
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2021-11-19 16:21:39 +0100

    CF overpower

--- Maven/src/main/java/Main.java
+++ Maven/src/main/java/Main.java
@@ -1,8 +1,10 @@
 
 public class Main {
 	public static void main(String args[]) {
-		CodiceFiscale c = new CodiceFiscale("RMPLCU00S30E507G".toCharArray());
-		System.out.println(c.isValid("Luca", "Rompani", new Data(30,11,2000), new Nazione("ITA"), new Comune("Mandello del Lario")));
+		CodiceFiscale l = new CodiceFiscale("RMPLCU00S30E507G".toCharArray());
+		System.out.println(l.isValid("Luca", "Rompani", new Data(30,11,2000), new Nazione("ITA"), new Comune("Lecco")));
 		
+		CodiceFiscale t = new CodiceFiscale("RMNTMS00R03F205E".toCharArray());
+		System.out.println(t.isValid("Tommaso", "Romano", new Data(3,10,2000), new Nazione("ITA"), new Comune("Milano")));
 	}
 }
