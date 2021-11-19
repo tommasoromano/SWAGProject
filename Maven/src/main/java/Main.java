985724bbd2751252b79cf848470b6fe57e05bf9f
rmplcu <rompa00@gmail.com>
2021-11-19 15:58:32 +0100

    Main

--- Maven/src/main/java/Main.java
+++ Maven/src/main/java/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
 	public static void main(String args[]) {
+		CodiceFiscale c = new CodiceFiscale("RMPLCU00S30E507G".toCharArray());
+		System.out.println(c.isValid("Luca", "Rompani", new Data(30,11,2000), new Nazione("ITA"), new Comune("Mandello del Lario")));
 		
 	}
 }
