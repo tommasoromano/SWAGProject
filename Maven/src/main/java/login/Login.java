0cf506e8a030fe0b1394ea9ac51fccb9338e452b
Tommaso Romano <info@tommasoromano.com>
2021-11-29 17:13:12 +0100

    up

---
+++ Maven/src/main/java/login/Login.java
@@ -0,0 +1,18 @@
+package login;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
