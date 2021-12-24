389354539c384f04509433b3d5fe75d005bc89e0
Tommaso Romano <info@tommasoromano.com>
2021-12-24 15:04:48 +0100

    up

--- Maven/src/main/java/root/util/DatiVoto.java
+++ Maven/src/main/java/root/util/DatiVoto.java
@@ -9,6 +9,12 @@ public class DatiVoto {
 	 * - voto ordinale e categorico c1:c2:c3:c4 ...
 	 * - voto categorico con preferenze c1(p1:p2):c2(p3:p4) ...
 	 * - referendum domanda
+	 * 
+	 * Quando esprimo un voto lo scrivo come
+	 * - voto ordinale cx(n):cx+1(n+1) ... 
+	 * - voto categorico cx
+	 * - voto categorico con preferenze cx(pn:pn+1)
+	 * - referendum si/no
 	 */
 	private String data;
 	
