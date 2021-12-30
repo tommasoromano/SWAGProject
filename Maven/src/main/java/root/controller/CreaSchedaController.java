e56b2bde8d3ed4ecfced344721f5b86a30c0e2a0
Luca Rompani <rompa00@gmail.com>
2021-12-30 18:45:18 +0100

    UI Crea scheda terminata

--- Maven/src/main/java/root/controller/CreaSchedaController.java
+++ Maven/src/main/java/root/controller/CreaSchedaController.java
@@ -2,15 +2,13 @@ package root.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.CheckBox;
-import org.controlsfx.control.CheckComboBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.FlowPane;
 import root.App;
 import java.util.List;
 import java.util.ArrayList;
@@ -19,12 +17,12 @@ import root.util.Data;
 import root.util.DatiVoto;
 import root.util.ModalitaConteggio;
 import root.util.ModalitaVoto;
-import root.util.Scheda;
 
 public class CreaSchedaController extends Controller {
 	
 	@Override
 	public void init() {
+		data = null;
 		String[] mv = ModalitaVoto.getAllTipi();
 		for (int i = 0; i < mv.length; i++) {
 			tipoVoto.getItems().add(mv[i]);
@@ -46,10 +44,12 @@ public class CreaSchedaController extends Controller {
 		
 	}
 	
+	private String data;
+	
 	private List<Node> removeNode;
 	
 	@FXML
-	private VBox parent;
+	private FlowPane container;
 	
     @FXML
     private Button buttonCrea;
@@ -64,9 +64,6 @@ public class CreaSchedaController extends Controller {
     private DatePicker dataInizio;
 
     @FXML
-    private TextArea datiVoto;
-
-    @FXML
     private TextField nomeScheda;
     
     @FXML
@@ -79,8 +76,6 @@ public class CreaSchedaController extends Controller {
 
     @FXML
     private ChoiceBox<String> tipoVoto;
-    
-    private ModalitaConteggio modalitaConteggio;
 
     @FXML
     void onActionCrea() {
@@ -90,8 +85,8 @@ public class CreaSchedaController extends Controller {
     	} 
     }
     
-    private boolean aggiungiNuovoVotabile(CheckComboBox<String> candidati, String tipo, String nome, String cognome, String partito) {
-    	String str = "";
+    private boolean aggiungiNuovoVotabile(ComboBox<String> candidati, String tipo, String nome, String cognome, String partito) {
+    	String item;
     	
     	if (tipo.equals("Gruppo")) {
     		if (nome == null || nome.isBlank()) {
@@ -99,7 +94,7 @@ public class CreaSchedaController extends Controller {
     			return false;
     		}
     		
-    		str = nome; 		
+    		item = nome; 		
     	} else if (tipo.equals("Candidato")) {
     		if (nome == null || nome.isBlank()) {
     			textError.setText("Compilare campo nome");
@@ -116,61 +111,171 @@ public class CreaSchedaController extends Controller {
     			return false;
     		}
     		
-    		str = nome + " " + cognome + "; " + partito;
+    		item = nome + " " + cognome + "; " + partito;
     	} else {
     		return false;
     	}
-    	candidati.getItems().add(str);
+    	candidati.getItems().add(item);
+    	
+    	//formato dati per il DB TODO
+    	// data += item;
+    	
     	return true;
     }
     
     private void cambiaModalitaVoto(String n) {
     	modalitaVoto = new ModalitaVoto(n);
-    	datiVoto.clear();
+    	container.getChildren().clear();
     	switch(modalitaVoto.getTipo()) {
     	 	case VotoOrdinale:
-    	 		candidatiGruppiUI();
+    	 		votoOrdinaleCategoricoUI();
     	 		break;
     	 	case VotoCategorico:
-    	 		candidatiGruppiUI();
+    	 		votoOrdinaleCategoricoUI();
     	 		break;
     	 	case VotoCategoricoConPreferenze:
-    	 		
-    	 		
+    	 		votoCategoricoPreferenzeUI();
     	 		break;
     	 	case Referendum:
-    	 		TextField txt = new TextField();
-    	 		txt.setPromptText("Domanda: ");
-    	 		break;
+    	 		Label txt = new Label("Domanda: ");
+    	 		txt.setMinWidth(100);
+    	 		txt.setMaxWidth(100);
+    	 		
+    	 		TextField domanda = new TextField();
+    	 		domanda.setMinWidth(300);
+    	 		domanda.setMinWidth(300);
+    	 		domanda.textProperty().addListener((obs, oldval, newval)->{
+    	 			data = newval;
+    	 		});
+    	 		
+    	 		container.getChildren().add(txt);
+    	 		container.getChildren().add(domanda);
     	}
     		
     }
     
-    private void candidatiGruppiUI() {
+    /**
+     * Crea e gestisce la UI per il voto categorico con preferenze
+     */
+    private void votoCategoricoPreferenzeUI() {
+    	ComboBox<String> gruppi = new ComboBox<>();  
+    	gruppi.setPromptText("Elenco dei gruppi");
+    	gruppi.setMaxWidth(300);
+ 		gruppi.setMinWidth(300);
+ 		container.getChildren().add(gruppi);
+ 		
+ 		TextField nomegruppo = new TextField();
+ 		nomegruppo.setPromptText("Nome gruppo: ");
+ 		nomegruppo.setMinWidth(100);
+ 		nomegruppo.setMaxWidth(100);
+ 		container.getChildren().add(nomegruppo);
+ 		
+ 		ComboBox<String> preferenze = new ComboBox<>();
+ 		preferenze.setPromptText("Elenco preferenze");
+ 		preferenze.setMinWidth(200);
+ 		preferenze.setMaxWidth(200);
+ 		container.getChildren().add(preferenze);
+ 		
+ 		TextField nomepref = new TextField();
+ 		nomepref.setPromptText("Nome:");
+ 		nomepref.setMinWidth(300);
+ 		nomepref.setMaxWidth(300);
+ 		container.getChildren().add(nomepref);
+ 		
+ 		TextField cognomepref = new TextField();
+ 		cognomepref.setPromptText("Cognome:");
+ 		cognomepref.setMinWidth(300);
+ 		cognomepref.setMaxWidth(300);
+ 		container.getChildren().add(cognomepref);
+ 		
+ 		Button addpref = new Button("Aggiungi preferenza");
+ 		addpref.setMinWidth(150);
+ 		addpref.setMaxWidth(150);
+ 		addpref.setOnAction((e)->{
+ 			if (nomepref == null || nomepref.getText().isBlank()) {
+ 				textError.setText("Inserire nome della preferenza");
+ 				return;
+ 			}
+ 			
+ 			if (cognomepref == null || cognomepref.getText().isBlank()) {
+ 				textError.setText("Inserire cognome della prefereza");
+ 				return;
+ 			}
+ 			preferenze.getItems().add(nomepref.getText() + " " + cognomepref.getText());
+ 			nomepref.setText("");
+ 			cognomepref.setText("");
+ 		});
+ 		container.getChildren().add(addpref);
+ 		
+ 		Button addgruppo = new Button("Aggiungi gruppo");
+ 		addgruppo.setMinWidth(150);
+ 		addgruppo.setMaxWidth(150);
+ 		container.getChildren().add(addgruppo);
+ 		addgruppo.setOnAction((e)->{
+ 			if (nomegruppo == null || nomegruppo.getText().isBlank()) {
+ 				textError.setText("Inserire nome del gruppo");
+ 				return;
+ 			}
+ 			
+ 			if (preferenze.getItems().size() == 0) {
+ 				textError.setText("Inserire almeno una preferenza");
+ 				return;
+ 			}
+ 			
+ 			String str = nomegruppo.getText() + "(";
+ 			
+ 			for (int i=0; i< preferenze.getItems().size();i++) {
+ 				str+= preferenze.getItems().get(i);
+ 				if (i<preferenze.getItems().size()-1) str+=",";
+ 			}
+ 			
+ 			str += ")";
+ 			
+ 			nomegruppo.setText("");
+ 			gruppi.getItems().add(str);
+ 			//formato dati per il db TODO
+ 			//data += str; .....
+ 		});
+    }
+    
+    private void votoOrdinaleCategoricoUI() {
     	removeNode = new ArrayList<>();
-    	CheckComboBox<String> candidati = new CheckComboBox<>();  	
-    	Label addnuovo = new Label("Aggiungi nuovo candidato o gruppo");
- 		ChoiceBox<String> addnew = new ChoiceBox<>();
+    	
+    	ComboBox<String> candidati = new ComboBox<>();  
+    	candidati.setPromptText("Elenco candidati");
+    	candidati.setMaxWidth(300);
+ 		candidati.setMinWidth(300);
+    	
+ 		ComboBox<String> addnew = new ComboBox<>();
+ 		addnew.setPromptText("Tipo candidato");
  		addnew.setOnAction((e) -> {
  			for (Node m : removeNode) {
- 				parent.getChildren().remove(m);
+ 				container.getChildren().remove(m);
  			}
  			removeNode = new ArrayList<>();
+ 			
  			TextField nome = new TextField();
  			nome.setPromptText("Nome: ");
- 			TextField cognome = new TextField();
+ 			nome.setMaxWidth(300);
+ 	 		nome.setMinWidth(300);
+ 			
+ 	 		TextField cognome = new TextField();
+ 			cognome.setMaxWidth(300);
+ 	 		cognome.setMinWidth(300);
  			cognome.setPromptText("Cognome: ");
-				TextField partito = new TextField();
-				partito.setPromptText("Partito: ");
-				parent.getChildren().add(3, nome);
-				removeNode.add(nome);
-				int pos = 4;
+			
+ 			TextField partito = new TextField();
+ 			partito.setMaxWidth(300);
+ 	 		partito.setMinWidth(300);
+			partito.setPromptText("Partito: ");
+			
+			container.getChildren().add(nome);
+			removeNode.add(nome);
  			if (addnew.getValue().equals("Candidato")) {
- 				parent.getChildren().add(4, cognome);
- 				parent.getChildren().add(5, partito);
+ 				container.getChildren().add(cognome);
+ 				container.getChildren().add(partito);
  				removeNode.add(cognome);
  				removeNode.add(partito);
- 				pos = 6;
  			}
  			Button ok = new Button("Inserisci");
  			ok.setId("b");
@@ -182,20 +287,15 @@ public class CreaSchedaController extends Controller {
 	 				partito.setText("");
  				}
  			});
- 			parent.getChildren().add(pos, ok);
+ 			container.getChildren().add(ok);
  			removeNode.add(ok);
  		});
+ 		
  		addnew.getItems().add("Gruppo");
  		addnew.getItems().add("Candidato");
- 		parent.getChildren().add(1, addnuovo);
- 		parent.getChildren().add(2, addnew);
- 		parent.getChildren().add(3,candidati);
+ 		container.getChildren().add(addnew);
+ 		container.getChildren().add(0,candidati);
     }
-    
-    private void cambiaModalitaConteggio(String n) {
-    	modalitaConteggio = new ModalitaConteggio(n);
-    }
-    
     
     private boolean checkVotoScrutinio() {
     	if (tipoVoto.getValue().toString().equals("Referendum")) {
@@ -248,11 +348,11 @@ public class CreaSchedaController extends Controller {
 			return false;
 		}
 		
-		//nome
-    	if (datiVoto.getText().length() == 0) {
-    		textError.setText("Compila il campo Dati voto");
+		//dati presenti
+		if (data==null || data.isBlank())  {
+			textError.setText("Missing data");
 			return false;
-    	}
+		}
 		
 		// cerca di creare scheda su DB
 		if (DBManager.getInstance().creaScheda(
@@ -260,7 +360,7 @@ public class CreaSchedaController extends Controller {
 				di, 
 				df, 
 				new ModalitaVoto(tipoVoto.getValue().toString()),
-				new DatiVoto(datiVoto.getText()), 
+				new DatiVoto(data), 
 				new ModalitaConteggio(tipoVincitore.getValue().toString()))) {
 			
     		return true;
